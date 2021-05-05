add9952895291269a2605d2b399ab99fb66836d9
michaelwijaya2810 <michaelwijaya.mw20@gmail.com>
2021-05-05 14:21:24 +0700

    Initial Commit

---
+++ app/src/main/java/com/mcs_project/mcs_lab_kuuf_project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mcs_project.mcs_lab_kuuf_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
